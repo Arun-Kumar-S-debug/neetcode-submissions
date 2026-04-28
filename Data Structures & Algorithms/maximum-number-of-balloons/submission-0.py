fb9eeedcf09843acb40a31c4ea6422fc462208a9
Arun-Kumar-S-debug <s.arunkumar7305@gmail.com>
2026-04-28 22:07:22 +0530

    Add: maximum-number-of-balloons - submission-0

---
+++ Data Structures & Algorithms/maximum-number-of-balloons/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        j=True
+        c=list("balloon")
+        text=list(text)
+        count=0
+        while j:
+            for i in c:
+                if i in text:
+                    text.remove(i)
+                else:
+                    j=False
+                    break
+            count+=1
+        return count-1
